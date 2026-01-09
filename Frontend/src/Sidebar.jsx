40d91f4ceae9bdc4a860fa7b3fff998c893a5ab9
Nilesh Sharma <nileshsharma9321@gmail.com>
2026-01-09 15:02:19 +0530

    changes

--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect, useCallback, useState } from "react";
 import { MyContext } from "./MyContext.jsx";
 import {v1 as uuidv1} from "uuid";
-import logo from "../assets/blacklogo.png";
+import logo from "./assets/blacklogo.png";
 
 function Sidebar() {
     const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
